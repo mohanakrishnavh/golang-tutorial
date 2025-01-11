195695f1e70f6122af829b61f328bd8a8ccc0e08
Mohanakrishna <mohanakrishnavh@gmail.com>
2025-01-11 12:05:51 -0800

    Adding addTask and printTasks functions

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go World")
+	// Declaring Variables
+	var appName = "MyTodoListApp"
+	// Slice - Dynamic, Array - Fixed Size
+	var tasks []string
+	// var limit = 20
+
+	fmt.Println("###### Welcome to Todolist App! :", appName, "######")
+	fmt.Println("### Tasks ###")
+
+	tasks = addTask(tasks, "Watch Go crash course")
+	tasks = addTask(tasks, "Watch Go full course")
+	tasks = addTask(tasks, "Reward myself with a nice meal")
+	printTasks(tasks)
+}
+
+func addTask(tasks []string, task string) []string {
+	tasks = append(tasks, task)
+	return tasks
+}
+
+func printTasks(tasks []string) {
+	// Loops
+	for idx, task := range tasks {
+		fmt.Printf("%d. %s\n", idx+1, task)
+	}
 }
